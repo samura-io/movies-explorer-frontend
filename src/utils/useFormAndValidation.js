87f374a8aa90ec849accffc46cfe9b18967ed552
Nikita Aleksankin <NTL5TIA@gmail.com>
2023-09-06 19:20:31 +0300

    добавил валидацию поля email

--- src/utils/useFormAndValidation.js
+++ src/utils/useFormAndValidation.js
@@ -1,18 +1,26 @@
 import { useState, useCallback, useEffect } from 'react';
 
 export default function useFormAndValidation() {
+  const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
   const [ values, setValues ] = useState({});
   const [ errors, setErrors ] = useState({});
   const [ isValid, setIsValid ] = useState(true);
 
   const handleChange = (e) => {
     const {name, value} = e.target
+    if (name === 'email' && !isEmailValid(value) ) {
+       e.target.setCustomValidity("Необходимо указать e-mail в формате name@domain.zone")
+      } else {
+        e.target.setCustomValidity("")
+      }
     setValues({...values, [name]: value });
     setErrors({...errors, [name]: e.target.validationMessage});
     setIsValid(e.target.closest('form').checkValidity());
   };
 
-
+  const isEmailValid = (value) => {
+    return EMAIL_REGEXP.test(value);
+  }
 
   const resetForm = useCallback((newValues = {}, newErrors = {}, newIsValid = false) => {
     setValues(newValues);
